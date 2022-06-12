6ad614453ccddebf945049f340073e0e91ff916f
KIM JUN GI <rlawns0528@naver.com>
2022-06-12 14:36:59 +0900

    test 2

--- javaserver/src/test/java/com/example/javaserver/JavaserverApplicationTests.java
+++ javaserver/src/test/java/com/example/javaserver/JavaserverApplicationTests.java
@@ -5,7 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class JavaserverApplicationTests {
-	// Commit Test
+	// Commit Test 2 
 	@Test
 	void contextLoads() {
 	}
